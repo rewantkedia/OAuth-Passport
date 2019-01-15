7a87e029375cc2008f8e919a9fbf7fa64849b53c
rewantkedia <kediarewant@gmail.com>
2019-01-15 22:35:42 +0530

    Mongodb connected

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const authRoute = require('./routes/auth_routes');
 const passportSetup = require('./config/passport-setup');
+const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 const app = express();
+mongoose.connect('mongodb://localhost/ninja');
 
 app.set('view engine','ejs');
-
 app.use('/auth',authRoute);
 app.get('/',(req,res)=>{
     // res.send({
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model');
 passport.use(
     new GoogleStrategy({
     //google strategy
@@ -9,6 +10,13 @@ passport.use(
         clientSecret: keys.google.clientSecret
     },(accessToken,refreshToken,profile,done)=>{
         //passport callbak function
-        console.log(profile);
-    })
+        console.log(profile);  
+        new User({
+            username: profile.displayName,
+            googleId: profile.id
+        }).save().then((user)=>{
+            console.log('New user logged ');
+            console.log(user);
+        })
+    }) 
 )
